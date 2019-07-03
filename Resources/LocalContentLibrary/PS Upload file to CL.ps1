ae9ccffdff9b3a81b18672b42c0250b1097a7813
yeseh <dev@jessewellenberg.nl>
2019-07-03 13:50:05 +0200

    Working V1 - windows authentication

--- Resources/LocalContentLibrary/PS Upload file to CL.ps1
+++ Resources/LocalContentLibrary/PS Upload file to CL.ps1
@@ -1,24 +1,51 @@
-﻿# USER CONFIGURATION
-$libname = "DBAChecks"
-$outputroot = "C:\Users\Jesse\Documents\Repo\QSContentlib\Resources\LocalContentLibrary"
-$method = "POST"
+﻿<#
+  POWERSHELL CONTENT LIBRARY UPLOADER - README
+  
+  This script calls the Qlik Sense Engine API to upload files from a local folder to a content library.
+  This script is mainly intended to enable the hosting of local JSON, HTML or other REST files on the Qlik Sense server without configuring an additional webserver.
+  
+  For this script to work, 3 variables need to be defined:
+    
+    1. $qsServer    -  The computer name or proxy path of your Qlik Sense server 
+    2. $inputFolder -  The path to the folder that serves as input for the 
+    3. $libname     -  The name of an already existing Qlik Sense content library that serves as the destination.
+    
+  
+    
+#>
 
-# REQUEST AUTHENTICATION & AUTHORISATION
-$xrfkey = "Xrfkey=12345678qwertyui"
-$hdrs = @{}
-$hdrs.add("X-Qlik-xrfkey", "12345678qwertyui")
-$hdrs.Add("X-Qlik-User","UserDirectory=JWIS;UserId=Jesse")
-$hdrs.add("Accept", "application/json")
-$cert = Get-childitem -Path "Cert:\CurrentUser\My" | where {$_.Subject -like '*QlikClient*'}
+#  REQUEST CONFIGURATION
 
-$url = "https://JWIS07QS:4242/qrs/$endpoint"
+## USER CONFIGURATION
+$qsServer = "JWIS07QS"                                                                    # Computer name or proxy address of the QS Server
+$inputfolder = "C:\Users\Jesse\Documents\Repo\QSContentlib\Resources\LocalContentLibrary" # Local folder path for input
+$libname = "DBAChecks"                                                                    # Name of existing QS content library to store output
 
-Get-ChildItem $outputroot -recurse | where {$_.extension -eq ".json"} | 
+## AUTHENTICATION & AUTHORISATION
+$hdrs = @{}                                                                               # Creation of request headers
+$xrfkey = "Xrfkey=12345678qwertyui"                                                       # XrfKey request url parameter
+$hdrs.add("X-Qlik-xrfkey", "12345678qwertyui")                                            # XrfKey header
+
+
+## GETTING AND STORING SESSION COOKIE FOR WINDOWS AUTHENTICATION
+$getUrl = "https://$qsServer/qrs/about?$xrfkey"
+Invoke-RestMethod -verbose -UseDefaultCredentials -Uri $geturl -Method Get -Headers $hdrs -SessionVariable websession
+$cookies = $websession.Cookies.GetCookies($geturl)
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$session.Cookies.Add($cookies);
+if($session) {write-host "[INFO] SESSION COOKIE STORED" -foregroundcolor green}              
+
+# EMPTY CONTENT LIBRARY
+
+
+
+# UPLOAD FILES FROM INPUT FOLDER TO CONTENTLIBRARY
+Get-ChildItem $inputfolder | where {$_.extension -eq ".json"} | 
 Foreach-Object {
     $content = $_.basename + $_.Extension
-    write-host $content -ForegroundColor yellow
-    $url = "https://JWIS07QS:4242/qrs/contentlibrary/$libname/uploadfile?externalpath=$content&$xrfkey"
-    write-host $url -ForegroundColor yellow
-    $response = Invoke-RestMethod -Uri $url -method $method -headers $hdrs -ContentType 'application/json' -Certificate $cert -verbose
+    $url = "https://$qsServer/qrs/contentlibrary/$libname/uploadfile?externalpath=$content&overwrite=true&$xrfkey"
+    if($content -and $url) {write-host "[INFO] URL CREATED FOR FILE $CONTENT" -ForegroundColor Green }
+    else {write-host "[ERROR] COULD NOT CREATE URL OR CONTENT" -foregroundcolor red}         
+    $response = Invoke-RestMethod -verbose -Uri $url -method post -headers $hdrs -Infile $content -ContentType  'application/json' -UseDefaultCredentials -websession $session #-Certificate $cert
 }
 
---
+++ Resources/Powershell/PS Write API Output to JSON.ps1
@@ -0,0 +1,28 @@
+# USER CONFIGURATION
+$timestamp = get-date -format "yyyyMMddHHmmss"
+$libname = "DBAChecks"
+$extension = "json"
+$filename = "GET_ContentLibraries_$timestamp.$extension"
+$outputroot = "C:\Users\Jesse\Documents\Repo\QSContentlib\Resources\LocalContentLibrary"
+$method = "get"
+$path = "$outputroot/$filename"
+
+# REQUEST AUTHENTICATION & AUTHORISATION
+$xrfkey = "Xrfkey=12345678qwertyui"
+$hdrs = @{}
+$hdrs.add("X-Qlik-xrfkey", "12345678qwertyui")
+$hdrs.Add("X-Qlik-User","UserDirectory=JWIS;UserId=Jesse")
+$hdrs.add("Accept", "application/json")
+$cert = Get-childitem -Path "Cert:\CurrentUser\My" | where {$_.Subject -like '*QlikClient*'}
+$endpoint = "contentlibrary?$xrfkey"
+$url = "https://JWIS07QS:4242/qrs/$endpoint"
+
+# GET ENDPOINTS
+Write-host "[INFO] Making call with URL $url" -ForegroundColor yellow
+$response = Invoke-RestMethod -Uri $url -method $method -headers $hdrs -ContentType 'application/json' -Certificate $cert
+
+# PROCESS RESPONSE
+
+Write-host "[INFO] Writing response to output file $path with timestamp $timestamp" -ForegroundColor cyan
+$response | Write-Output | OUT-file -filepath $path 
+Write-host "[OPERATION COMPLETE]" -ForegroundColor green
