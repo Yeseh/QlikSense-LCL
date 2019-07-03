ed241ec33dcd127bd526d6574bbdbe6ff419f958
yeseh <dev@jessewellenberg.nl>
2019-07-03 16:09:07 +0200

    no message

--- Resources/Powershell/PS Upload file to CL.ps1
+++ Resources/Powershell/PS Upload file to CL.ps1
@@ -3,7 +3,8 @@
   
   This script calls the Qlik Sense Engine API to upload files from a local folder to a content library.
   This script is mainly intended to enable the hosting of local JSON, HTML or other REST files on the Qlik Sense server without configuring an additional webserver.
-  
+  The script first gets API info to establish a user session. Then the existing contentlibrary is emptied and the files in the local storages are reuploaded.
+
   For this script to work, 3 variables need to be defined:
     
     1. $qsServer    -  The computer name or proxy path of your Qlik Sense server 
@@ -44,16 +45,17 @@ $getUrl = "https://$qsServer/qrs/staticcontent/enumeratefiles?path=/content/$lib
 $contentEnum = Invoke-RestMethod -verbose -Uri $getUrl -method get -headers $hdrs -ContentType  'application/json' -UseDefaultCredentials -websession $session 
 if($contentEnum) {write-host "[INFO] CONTENT FETCHED FOR CONTENTLIBRARY $libname" -ForegroundColor Green }
 
-## EMPTY CONTENT LIBRARY
+## EMPTY CONTENT LIBRARY IF IT IS NOT EMPTY
+if($contentEnum){
 foreach($i in $contentEnum){
     write-host $i.path -ForegroundColor Yellow
     $deleteContent = [System.io.path]::GetFileName($i.path)
     $deleteUrl = "https://$qsServer/qrs/contentlibrary/$libname/deletecontent?externalpath=$deletecontent&$xrfkey"
     if($deletecontent -and $posturl) {write-host "[INFO] FILE $deleteContent successfully deleted" -ForegroundColor magenta }
     Invoke-RestMethod -verbose -Uri $deleteurl -method delete -headers $hdrs  -ContentType  'application/json' -UseDefaultCredentials -websession $session
+    }
 }
 
-<#
 # UPLOAD FILES FROM INPUT FOLDER TO CONTENTLIBRARY
 Get-ChildItem $inputfolder | where {($_.extension -eq ".json") -and !($_.BaseName -eq "content")} | 
 Foreach-Object {
@@ -63,5 +65,4 @@ Foreach-Object {
     else {write-host "[ERROR] COULD NOT CREATE URL OR CONTENT" -foregroundcolor red}         
     Invoke-RestMethod -verbose -Uri $postUrl -method post -headers $hdrs -Infile $postcontent -ContentType  'application/json' -UseDefaultCredentials -websession $session
 }
-#>
 
